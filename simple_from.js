b9d2190f3abb768795707332be49f7acf8e00d79
Themen <thesolve@mail.ru>
2023-12-19 13:32:50 +0300

    У простых программ сделал вывод в файл, а не в консоль

--- simple_from.js
+++ simple_from.js
@@ -1,4 +1,4 @@
 'use strict';
 const fs = require('fs');
 const data = fs.readFileSync(process.argv[2]).toString().split('\n')[0].replace(/\r/g, '');
-process.stdout.write(Buffer.from(data, 'base64'));
+fs.writeFileSync('data', Buffer.from(data, 'base64'));
